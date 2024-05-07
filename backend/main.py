5e25144a9b6a55cdd249bf07baaa081acec44266
hoang-danny05 <bajanboss132@gmail.com>
2024-05-07 12:53:01 -0700

    added CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
 import sys
 import traceback
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mecademicpy import rbt
 from mecademicpy import robot_classes
 
-app= FastAPI()
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 robot: rbt.Robot = rbt.Robot()
 
